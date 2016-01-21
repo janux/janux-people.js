0d8dc54f54f2d4886686c8a90ad84dbb138b7e06
hielo <alejandro@espaciomexico.net>
2016-01-20 20:54:14 -0600

    className method in tools

---
+++ src/tools.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Return class name of target
+exports.className = function className(target){
+	var funcNameRegex = /function (.{1,})\(/;
+	var results  = (funcNameRegex).exec(target["constructor"].toString());
+	return (results && results.length > 1) ? results[1] : "";
+};
+
+// Create mixin
+exports.extend = function extend(destination, source) {
+	for (var k in source) {
+		if (source.hasOwnProperty(k)) {
+			destination[k] = source[k];
+		}
+	}
+	return destination;
+};
