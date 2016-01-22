9e6b92e1f9b43a5a4553cb8b1ad55ab2172b0504
Alejandro Alvarez <alejandro@espaciomexico.net>
2016-01-21 19:17:42 -0600

    update exposed modules

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
-// example of exposing modules
-exports.Calculator    = require('./src/classExample').Calculator;
-
+// exposing modules
+exports.Person    = require('./dist/impl/Person').PersonImpl;
+exports.Organization = require('./dist/impl/Organization').OrganizationImpl;
+exports.ContactMethodManager = require('./dist/impl/ContactMethodManager').ContactMethodManager;
+exports.PhoneNumber = require('./dist/impl/PhoneNumber').PhoneNumberImpl;
+exports.PostalAddress = require('./dist/impl/PostalAddress').PostalAddressImpl;
