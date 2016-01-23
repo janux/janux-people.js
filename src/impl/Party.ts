dcef7834d3501c540c57fde65490da8854b41112
hielo <alejandro@espaciomexico.net>
2016-01-22 19:38:45 -0600

    Contact Method Manager methods available in PartyAbstract

--- src/impl/Party.ts
+++ src/impl/Party.ts
@@ -17,9 +17,6 @@ import {PostalAddress} from '../api/geography/PostalAdress';
 import {Uri} from '../api/net/Uri';
 import {Url} from '../api/net/Url';
 
-// implementations
-import {ContactMethodManager} from './ContactMethodManager';
-
 /**
  ***************************************************************************************************
  * Base implementation for the class hierarchy to which a Person and an Organization belong
@@ -30,96 +27,100 @@ import {ContactMethodManager} from './ContactMethodManager';
  */
 export abstract class PartyAbstract implements Party
 {
-	private code: string;
-	private contactMan: ContactMethodManager = new ContactMethodManager();
-	//private fopList: List<FormOfPayment>;
+	public code: string; // Optional unique business identifier for this Party
+	private contactMethods: Dictionary<string, ContactMethod>;
 
-	/** Optional unique business identifier for this Party */
-	getCode(): string {
-		return this.code;
-	}
-
-	setCode(code: string): void {
-		this.code = code;
+	constructor() {
+		this.contactMethods = new Dictionary<string, ContactMethod>();
 	}
 
 	abstract getPartyName(): PartyName;
 
-	/**
-	 * Telephone numbers keyed by a string code representing a user-defined type of
-	 * Phone Number, such as PHYSICAL, BILLING, etc...
-	 */
-	getContactMethods(): Dictionary<string, ContactMethod> {
-		if (!(this.contactMan instanceof ContactMethodManager)) {
-			console.warn("getContactMethods method is having to create another instance of contactMan - somehow it wasn't created with constructor");
-			this.contactMan = new ContactMethodManager();
+	getContactMethod(aType: string): ContactMethod {
+
+		// Get contact for a specific type Ej: Home
+		var findContact =  this.contactMethods.getValue(aType); // contactDict.getValue(aType);
+
+		if (findContact != null) {
+			return findContact;
 		}
-		return this.contactMan.getContactMethods();
+		else{
+			return (null);
+		}
 	}
 
-	setContactMethods(contactMethods: Dictionary<string, ContactMethod>): void {
-		this.contactMan.setContactMethods(contactMethods);
+	setContactMethod(type: string, contactMethod: ContactMethod): void {
+		this.contactMethods.setValue(type, contactMethod);
+		console.log("added contact method of type '" + type + "'");
 	}
 
-	getContactMethod(kind: string): ContactMethod {
-		return this.contactMan.getContactMethod(kind);
-	}
-
-	setContactMethod(kind: string, cMethod: ContactMethod): void {
-		this.contactMan.setContactMethod(kind, cMethod);
-	}
-
-	/**
+	/*
 	 * Postal mailing addresses keyed by a string code representing a
 	 * user-defined type of ContactMethod kind, such as PHYSICAL_ADDRESS,
-	 * CHECK-IN_ADDRESS, MAILING_ADDRTestCaseESS, BILLING_ADDRESS, etc...
+	 * CHECK-IN_ADDRESS, MAILING_ADDRESS, BILLING_ADDRESS, etc...
 	 */
 	getPostalAddresses(): Dictionary<string, ContactMethod> {
-		return this.contactMan.getPostalAddresses();
+		return this.createContactMethodDictionary('addresses');
 	}
 
 	getPostalAddress(kind: string): PostalAddress {
-		return this.contactMan.getPostalAddress(kind);
+		return <PostalAddress>this.getContactMethod(kind);
 	}
 
-	/**
+	/*
 	 * Telephone numbers keyed by a string code representing a user-defined type of
 	 * Phone Number, such as PHYSICAL_PHONE, BILLING_PHONE, etc...
 	 */
 	getPhoneNumbers(): Dictionary<string, ContactMethod> {
-		return this.contactMan.getPhoneNumbers();
+		return this.createContactMethodDictionary('phoneNumbers');
 	}
 
 	getPhoneNumber(kind: string): PhoneNumber {
-		return this.contactMan.getPhoneNumber(kind);
+		return <PhoneNumber>this.getContactMethod( kind);
 	}
 
-	/**
+	/*
 	 * Email addresses keyed by a string code representing a user-defined kind of
 	 * Email, such as EMAIL1, INFO_EMAIL etc...
 	 */
 	getEmailAddresses(): Dictionary<string, ContactMethod> {
-		return this.contactMan.getEmailAddresses();
+		return this.createContactMethodDictionary('emails');
 	}
 
 	getEmailAddress(kind: string): Uri {
-		return this.contactMan.getEmailAddress(kind);
+		return <Uri>this.getContactMethod( kind);
+	}
+
+	///*
+	// * Uniform Resource Locators (eg web page or ftp addresses) keyed by a string
+	// * code representing a user-defined type of URL such as WEB_SITE, INTRANET, etc...
+	// */
+	//getUrls():Dictionary {
+	//	return this.createContactMethodDictionary(Url.class);
+	//}
+	//
+	//getUrl(kind: string): Url {
+	//	return <Url>this.getContactMethod(kind);
+	//}
+
+	/** creates a Dictionary for each subclass of ContactMethod found in the main ContactMethod Dictionary */
+	private createContactMethodDictionary(aClassName): Dictionary<string, ContactMethod> {
+		var contacts: Dictionary<string, ContactMethod> = new Dictionary<string, ContactMethod>();
+
+		//this.contactMethods.forEach((methodKey: string, cMethod: ContactMethod)=>{
+		//	if (cMethod.typeName == aClassName) {
+		//		contacts.setValue(methodKey, cMethod);
+		//	}
+		//});
+
+		contacts = this.contactMethods;
+
+		console.debug("recreated contact method Dictionary");
+		return (contacts);
 	}
 
 	toString(): string {
 		// Short hand. Adds each own property
 		return collections.makeString(this);
 	}
-
-	///**
-	// * Uniform Resource Locators (eg web page or ftp addresses) keyed by a string
-	// * code representing a user-defined type of URL such as WEB_SITE, INTRANET, etc...
-	// */
-	//getUrls(): Dictionary {
-	//	return this.contactMan.getUrls();
-	//}
-	//
-	//getUrl(kind: string): Url {
-	//	return this.contactMan.getUrl(kind);
-	//}
 } // end class PartyAbstract
