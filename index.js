78888a72d3fd34dfe6763b08b9aa690ede4e7990
hielo <alejandro@espaciomexico.net>
2016-01-24 19:11:54 -0600

    delete ContactMethodManager of exposed modules list

--- index.js
+++ index.js
@@ -3,6 +3,5 @@
 // exposing modules
 exports.Person    = require('./dist/impl/Person').PersonImpl;
 exports.Organization = require('./dist/impl/Organization').OrganizationImpl;
-exports.ContactMethodManager = require('./dist/impl/ContactMethodManager').ContactMethodManager;
 exports.PhoneNumber = require('./dist/impl/PhoneNumber').PhoneNumberImpl;
 exports.PostalAddress = require('./dist/impl/PostalAddress').PostalAddressImpl;
