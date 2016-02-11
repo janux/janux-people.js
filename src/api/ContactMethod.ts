057e8069ca652bdb7c2bb7c5c8ef935e52dcaa42
hielo <alejandro@espaciomexico.net>
2016-02-10 19:30:58 -0600

    Contact Method base fields according portable contacts

--- src/api/ContactMethod.ts
+++ src/api/ContactMethod.ts
@@ -1,5 +1,22 @@
 export interface ContactMethod // extends Persistent, Serializable, Cloneable
 {
-	typeName: string;
+	/**
+	 * Portable Contacts Plural Fields: phoneNumbers, emails, etc.
+	 **/
+	field: string;
+
+	/**
+	 * The type of field for this instance, usually used to label the preferred
+	 * function of the given contact information. Unless otherwise specified,
+	 * this string value specifies Canonical Values of work, home, and other.
+	 **/
+	type: string;
+
+	/**
+	 * A Boolean value indicating whether this instance of the Plural Field
+	 * is the primary or preferred value of for this field
+	 **/
+	primary: boolean;
+
 	toString(): string;
 }
