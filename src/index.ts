203a6b44ce212730f14d668d5598756aef58dcda
Philippe Paravicini <philippe@januxmedia.com>
2026-03-30 14:43:02 -0700

    Updated build to use npm scripts and be based on typescript-5

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { PersonImpl } from './impl/Person';
+export { OrganizationImpl } from './impl/Organization';
+export { PhoneNumberImpl } from './impl/PhoneNumber';
+export { PostalAddressImpl } from './impl/PostalAddress';
+export { EmailAddressImpl } from './impl/EmailAddress';
+export { PartyAbstract } from './impl/Party';
+export { Party } from './api/Party';
