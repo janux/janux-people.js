15ded0046e94f8aa372accb4f66c705754e668a6
Philippe Paravicini <philippe@januxmedia.com>
2026-03-31 09:36:14 -0700

    Phase 8: fix noImplicitAny errors

--- src/tools.ts
+++ src/tools.ts
@@ -1,12 +1,12 @@
 'use strict';
 
 // Return class name of target
-export function className(target): string {
-	return target?.constructor?.name ?? '';
+export function className(target: object): string {
+	return (target as { constructor?: { name?: string } })?.constructor?.name ?? '';
 }
 
 // Create mixin
-export function extend(destination, source) {
+export function extend(destination: Record<string, unknown>, source: Record<string, unknown>) {
 	for (const k in source) {
 		if (Object.prototype.hasOwnProperty.call(source, k)) {
 			destination[k] = source[k];
